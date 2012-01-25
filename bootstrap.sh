7f738f582ada343c28552c2c26c28a22723fde29
robbles <odwyerrob@gmail.com>
2012-01-24 16:12:39 -0800

    Make this folder compatible with Eclipse (PyDev)

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,10 @@
 # Exit this script on error
 set -e
 
+sudo apt-get update
+
 # Install ubuntu packages
-sudo apt-get install build-essential python-pip python-dev python-mysqldb git-core sqlite3 nginx vim
+sudo apt-get install --assume-yes build-essential python-pip python-dev python-mysqldb git-core sqlite3 nginx vim
 
 # Install python packages
 sudo pip install -r requirements.txt
