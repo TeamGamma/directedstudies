908164eeca63e49ecc0898369f87f623dbc8f882
robbles <odwyerrob@gmail.com>
2012-01-31 14:04:17 -0800

    update bootstrap script to use apache instead of nginx

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ set -e
 sudo apt-get update
 
 # Install ubuntu packages
-sudo apt-get install --assume-yes build-essential python-pip python-dev python-mysqldb git-core sqlite3 nginx vim
+sudo apt-get install --assume-yes build-essential python-pip python-dev python-mysqldb git-core sqlite3 apache2 libapache2-mod-wsgi
 
 # Install python packages
 sudo pip install -r requirements.txt
