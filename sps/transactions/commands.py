753ff4893e05d539dcb7c362e1c89aa098ed079d
robbles <odwyerrob@gmail.com>
2012-02-04 23:53:17 -0800

    convert commands into classes

--- sps/transactions/commands.py
+++ sps/transactions/commands.py
@@ -1,12 +1,50 @@
 
-# TODO: make these into classes (for callbacks, etc.)
-
-def echo(client, msg):
-    return msg
-
-def uppercase(client, msg):
-    return msg.upper()
 
 class CommandError(Exception):
     pass
+
+
+class UnknownCommandError(CommandError):
+    pass
+
+
+class InvalidCommandArgumentsError(CommandError):
+    pass
+
+
+class CommandHandler(object):
+    # Associates command labels (e.g. BUY) to subclasses of CommandHandler
+    commands = {}
+
+    @classmethod
+    def get_handler(cls, label):
+        """ Finds the CommandHandler associated with label """
+        if label not in cls.commands:
+            raise UnknownCommandError(label)
+
+        return cls.commands[label]()
+
+    @classmethod
+    def register_command(cls, label, command):
+        """
+        Registers command with CommandHandler so it can be looked up by label
+        """
+        cls.commands[label] = command
+
+
+class EchoCommand(CommandHandler):
+    """ Echoes a single argument back to the client """
+    def run(self, message):
+        return message + '\n'
+
+CommandHandler.register_command('ECHO', EchoCommand)
+
+
+class UppercaseCommand(CommandHandler):
+    """ Like ECHO, but returns the message in uppercase """
+    def run(self, message):
+        return message.upper() + '\n'
+
+CommandHandler.register_command('UPPER', UppercaseCommand)
+
 
--- sps/transactions/server.py
+++ sps/transactions/server.py
@@ -5,16 +5,10 @@ import eventlet
 import eventlet.patcher
 eventlet.patcher.monkey_patch()
 
-from sps.transactions import commands
+from sps.transactions.commands import CommandHandler, CommandError, UnknownCommandError
 from sps.transactions import database
 
 class TransactionServer(object):
-    # TODO: make CommandHandler and use method lookup instead
-    commands = {
-        'ECHO': commands.echo,
-        'UPPERCASE': commands.uppercase,
-    }
-
     def handle(self, client, address):
         """ Handles a single client """
 
@@ -23,23 +17,24 @@ class TransactionServer(object):
         # Read multiple lines from client and parse command
         while True:
             line = client.recv(1000)
+
             if not line:
                 print 'Client disconnected'
                 return
-            try:
-                print 'Received:' + repr(line.split(' ', 1))
-                command, args = line.split(' ', 1)
-            except ValueError, e:
-                self.error(client, 'Invalid command - %s' % e)
-                return
+
+            print 'Received: %s' % repr(line)
+            tokens = line[:-1].split(' ')
+            command = tokens[0]
+            args = tokens[1:]
 
             try:
-                response = self.commands[command](client, args)
-            except KeyError:
+                handler = CommandHandler.get_handler(command)
+                response = handler.run(*args)
+            except UnknownCommandError:
                 self.error(client, 'Command does not exist')
                 return
-            except commands.CommandError, e:
-                self.error(client, e)
+            except TypeError, e:
+                self.error(client, 'Incorrect arguments for command "%s"' % command)
                 return
             except Exception, e:
                 print 'Unexpected error: %s' % e
@@ -57,9 +52,6 @@ def run_server(port):
     # Setup server
     server = eventlet.listen(('0.0.0.0', port))
     transaction_server = TransactionServer()
-
-    # Setup database pool
-    session = database.get_session()
 
     print >> sys.stderr, 'Running transaction server on port %d' % port
     eventlet.serve(server, transaction_server.handle)
