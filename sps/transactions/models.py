1385b69c63302b44b269b836e4778af831d1b5f6
robbles <odwyerrob@gmail.com>
2012-02-14 22:10:45 -0800

    fixed table names and dollars/cents for other models

--- sps/transactions/models.py
+++ sps/transactions/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import composite
 from collections import namedtuple
 
@@ -25,56 +25,69 @@ class Money(namedtuple('Money', 'dollars cents')):
 
 
 class Client(Base):
-    __tablename__ = 'Client'
+    __tablename__ = 'client'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     username = Column(String(50))
     password = Column(String(50))
-    account_balance_dollars = Column(Integer)
-    account_balance_cents = Column(Integer)
-    reserve_balance_dollars = Column(Integer)
-    reserve_balance_cents = Column(Integer)
+    _account_balance_dollars = Column(Integer)
+    _account_balance_cents = Column(Integer)
+    _reserve_balance_dollars = Column(Integer)
+    _reserve_balance_cents = Column(Integer)
 
-    account_balance = composite(Money, account_balance_dollars, account_balance_cents)
-    reserve_balance = composite(Money, reserve_balance_dollars, reserve_balance_cents)
+    # Automatically convert between Money and dollars/cents columns
+    account_balance = composite(Money, _account_balance_dollars, _account_balance_cents)
+    reserve_balance = composite(Money, _reserve_balance_dollars, _reserve_balance_cents)
 
     def __init__(self, username, password, account_balance, reserve_balance):
         self.username = username
         self.password = password
-        self.account_balance_dollars = account_balance.dollars
-        self.account_balance_cents = account_balance.cents
-        self.reserve_balance_dollars = reserve_balance.dollars
-        self.reserve_balance_cents = reserve_balance.cents
+        self._account_balance_dollars = account_balance.dollars
+        self._account_balance_cents = account_balance.cents
+        self._reserve_balance_dollars = reserve_balance.dollars
+        self._reserve_balance_cents = reserve_balance.cents
 
     def __repr__(self):
        return "<Client('%s', balance=%s)>" % (self.username, self.account_balance)
 
 
 class Query(Base):
-    __tablename__ = 'Query Transactions'
+    __tablename__ = 'queries'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     user_id  = Column(Integer)
     stock_symbol = Column(String(7))
-    stock_value  = Column(Float)
+    _stock_value_dollars  = Column(Integer)
+    _stock_value_cents  = Column(Integer)
+    stock_value = composite(Money, _stock_value_dollars, _stock_value_cents)
     crypto_key   = Column(String(50))
-    query_fee    = Column(Float)
+    _query_fee_dollars    = Column(Integer)
+    _query_fee_cents    = Column(Integer)
+    query_fee = composite(Money, _query_fee_dollars, _query_fee_cents)
     timestamp    = Column(String(50))
 
-    def __init__(self):
-        pass
+    def __init__(self, user_id, stock_symbol, stock_value, crypto_key, query_fee, timestamp):
+        self.user_id = user_id
+        self.stock_symbol = stock_symbol
+        self.stock_value = stock_value
+        self.crypto_key = crypto_key
+        self.query_fee = query_fee
+        self.timestamp = timestamp
 
 
 class Transaction(Base):
-    __tablename__ = 'Buy/Sell Transactions'
+    __tablename__ = 'transactions'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     user_id  = Column(Integer)
     stock_symbol = Column(String(7))
-    stock_value  = Column(Float)
+    _stock_value_dollars  = Column(Integer)
+    _stock_value_cents  = Column(Integer)
     operation    = Column(String(3))
     quantity     = Column(Integer)
-    broker_fee   = Column(Float)
+    _broker_fee_dollars   = Column(Integer)
+    _broker_fee_cents   = Column(Integer)
+    broker_fee   = composite(Money, _broker_fee_dollars, _broker_fee_cents)
     processing_time = Column(String(50))
     timestamp    = Column(String(50))
 
@@ -84,15 +97,19 @@ class Transaction(Base):
 
 
 class SetTransaction(Base):
-    __tablename__ = 'Set Buy/Sell Transactions'
+    __tablename__ = 'set_transactions'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     user_id  = Column(Integer)
     stock_symbol = Column(String(7))
-    stock_value  = Column(Float)
+    _stock_value_dollars  = Column(Integer)
+    _stock_value_cents  = Column(Integer)
+    stock_value  = composite(Money, _stock_value_dollars, _stock_value_cents)
     operation    = Column(String(3))
     quantity     = Column(Integer)
-    broker_fee   = Column(Float)
+    _broker_fee_dollars   = Column(Integer)
+    _broker_fee_cents   = Column(Integer)
+    broker_fee   = composite(Money, _broker_fee_dollars, _broker_fee_cents)
     processing_time = Column(String(50))
     query_times  = Column(Integer)
     timestamp    = Column(String(50))
