d79f7dcb955fe6e12d6b3d3d13ba646a72395c46
robbles <odwyerrob@gmail.com>
2012-02-14 21:45:46 -0800

    refactored models.Money, made Client use dollars/cents

--- sps/transactions/models.py
+++ sps/transactions/models.py
@@ -1,11 +1,28 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.orm import composite
 from collections import namedtuple
 
 Base = declarative_base()
 
-# Used to represent money in the system without floating point errors
-Money = namedtuple('Money', 'dollars cents')
+class Money(namedtuple('Money', 'dollars cents')):
+    """ 
+    Used to represent money in the system without floating point errors.
+    Note: Money objects are immutable.
+
+    >>> m1 = Money(dollars=10, cents=20)
+    >>> m1.dollars, m1.cents
+    (10, 20)
+
+    >>> m2 = Money(10, 20)
+    >>> m1 == m2
+    True
+    """
+    __slots__ = ()
+
+    def __composite_values__(self):
+        return (self[0], self[1])
+
 
 class Client(Base):
     __tablename__ = 'Client'
@@ -13,14 +30,21 @@ class Client(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     username = Column(String(50))
     password = Column(String(50))
-    account_balance = Column(Integer)
-    reserve_balance = Column(Integer)
+    account_balance_dollars = Column(Integer)
+    account_balance_cents = Column(Integer)
+    reserve_balance_dollars = Column(Integer)
+    reserve_balance_cents = Column(Integer)
 
-    def __init__(self, username, password, account_balance=0):
+    account_balance = composite(Money, account_balance_dollars, account_balance_cents)
+    reserve_balance = composite(Money, reserve_balance_dollars, reserve_balance_cents)
+
+    def __init__(self, username, password, account_balance, reserve_balance):
         self.username = username
         self.password = password
-        self.account_balance = account_balance
-	self.reserve_balance = 0
+        self.account_balance_dollars = account_balance.dollars
+        self.account_balance_cents = account_balance.cents
+        self.reserve_balance_dollars = reserve_balance.dollars
+        self.reserve_balance_cents = reserve_balance.cents
 
     def __repr__(self):
        return "<Client('%s', balance=%s)>" % (self.username, self.account_balance)
