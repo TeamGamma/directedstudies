8f552bd471632d98d1ee774def949cfc261d3b43
Alim Jiwa <alimjiwa@gmail.com>
2012-02-17 15:16:02 -0800

    added more error checking to the client side form

--- sps/web/app.py
+++ sps/web/app.py
@@ -1,53 +1,66 @@
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
 
+import transaction_interface
+
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
     if request.method == "POST":
 
-        if ( checkentry( request.form['action'],
+        trans_server_message = checkentry( 
+                    request.form['username'],
+                    request.form['action'],
         			request.form['money value'],
                    	request.form['stock quantity'],
                    	request.form['set point'],
-		        	request.form['stock symbol'])) == True: 
-
+		        	request.form['stock symbol'])
         
-            return  ( 	""" method: %s <br><br>
-                            input monetary value: %s <br>
-                            stock quantity: %s <br>
-                            stock set point: %s <br>
-                            stock symbol %s <br> """ % (
+        if trans_server_message != False: 
+            
+            transaction_interface.send('localhost',50008, trans_server_message)
+        
+            return  ( 	""" 
+                            Hi                      %s ! <br><br>
+                            Method:                 %s <br><br>
+                            Input monetary value:   %s <br>
+                            Stock quantity:         %s <br>
+                            Stock set point:        %s <br>
+                            Stock symbol            %s <br> """ % (
                             
+                            request.form['username'],
                             request.form['action'],
                             request.form['money value'],
                             request.form['stock quantity'],
                             request.form['set point'],
                             request.form['stock symbol'] ))
                             
-        else: return ('You fudged it up, big boy')
+        else: return ('You fudged it up, big boy <br><br> Go back and try again')
     
 
     else:
         return render_template('form.html')
 
 
-def checkentry(action, money_value, stock_quantity, set_point, stock_symbol):
+def checkentry(username, action, money_value, stock_quantity, set_point, stock_symbol):
 
-    # check each action for constraints
+    # Check username for validity
+    if len(username)==0:return False
+    
+    # Check each action for constraints
     if action=='add money':
         if  len(money_value)==0: return False
         
-        #check to see if they entered a number
+        #Check to see if they entered a number
         try:
-            if float(money_value)>0 : return True
+            if float(money_value)>0 : return 'ADD,'+ username + ',' + money_value 
             else: return False
         except ValueError:
             return False          
         
     elif action=='get quote':
-        if len(stock_symbol)!=0: return True
+        if len(stock_symbol)!=0: return 'QUOTE' + username + ',' + stock_symbol 
         else: return False
 
     elif action=='buy shares':
@@ -55,15 +68,15 @@ def checkentry(action, money_value, stock_quantity, set_point, stock_symbol):
     elif action=='sell shares':
         return True
     elif action=='set automated point':
-        return Trure       
+        return True       
     elif action=='review transaction list':
         return True
     elif action=='cancel transaction':
-        return True
-    elif action=='commit transaction':
-        return True
-    else: return True
+        if len(username)!=0: return 'CANCEL_BUY,' +username
 
+    elif action=='commit transaction':
+        if len(username)!=0: return 'COMMIT_BUY,' +username
+    else: return False
 
 
 @app.route("/some/other/path/")
--- sps/web/transaction_interface.py
+++ sps/web/transaction_interface.py
@@ -1,16 +1,20 @@
 import socket
 import sys
 
-HOST = 'localhost' # the remote host
-PORT = 50008
+#HOST = 'localhost' # the remote host
+#PORT = 50008
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect ((HOST, PORT))
+def send(HOST, PORT, message):
+ 
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect ((HOST, PORT))
 
-s.sendall('Hello World')
+    s.sendall(message)
 
-data = s.recv(1024)
-s.close()
-print 'Received', repr(data)
+    confirmation_msg = s.recv(1024)
+    s.close()
+    print 'Received', repr(confirmation_msg)
+    return True
 
-
+if __name__ =='__main__':
+    print 'you just ran the transaction interface'
