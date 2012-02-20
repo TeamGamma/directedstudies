ad1764fb5ec81b3afc0577be6d5a8497f900d840
robbles <odwyerrob@gmail.com>
2012-02-19 20:43:35 -0800

    changes to money and user, base command tests

--- sps/database/models.py
+++ sps/database/models.py
@@ -8,7 +8,8 @@ Base = declarative_base()
 class Money(namedtuple('Money', 'dollars cents')):
     """ 
     Used to represent money in the system without floating point errors.
-    Note: Money objects are immutable.
+    Note: Money objects are immutable 
+    (you can't assign dollars and cents directly)
 
     >>> m1 = Money(dollars=10, cents=20)
     >>> m1.dollars, m1.cents
@@ -17,18 +18,60 @@ class Money(namedtuple('Money', 'dollars cents')):
     >>> m2 = Money(10, 20)
     >>> m1 == m2
     True
+
+    >>> money = Money(dollars=45, cents=67)
+    >>> str(money)
+    '45.67'
+
+    >>> money = Money(dollars=45, cents=0)
+    >>> str(money)
+    '45.00'
+
+    >>> money_str = "1.43"
+    >>> money = Money.from_string(money_str)
+    >>> money.dollars, money.cents
+    (1, 43)
+
+    >>> money_str = "65"
+    >>> money = Money.from_string(money_str)
+    >>> money.dollars, money.cents
+    (65, 0)
     """
     __slots__ = ()
 
     def __composite_values__(self):
         return (self[0], self[1])
 
+    def __add__(self, money):
+        return Money(self.dollars + money.dollars, self.cents + money.cents)
 
-class Client(Base):
-    __tablename__ = 'client'
+    def __sub__(self, money):
+        return Money(self.dollars - money.dollars, self.cents - money.cents)
+
+    def __str__(self):
+        """ 
+        Converts a Money object into a string representation.
+        """
+        return "%d.%02d" % (self.dollars, self.cents)
+
+    @staticmethod
+    def from_string(money_str):
+        """
+        Converts the string representation of money (e.g. "1.43") into a Money
+        """
+        if '.' in money_str:
+            dollars, cents = map(int, money_str.split('.'))
+        else:
+            dollars, cents = int(money_str), 0
+        return Money(dollars, cents)
+
+
+
+class User(Base):
+    __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    username = Column(String(50))
+    userid = Column(String(50))
     password = Column(String(50))
     _account_balance_dollars = Column(Integer)
     _account_balance_cents = Column(Integer)
@@ -39,32 +82,36 @@ class Client(Base):
     account_balance = composite(Money, _account_balance_dollars, _account_balance_cents)
     reserve_balance = composite(Money, _reserve_balance_dollars, _reserve_balance_cents)
 
-    def __init__(self, username, password, account_balance, reserve_balance):
-        self.username = username
+    def __init__(self, userid, password, account_balance=None, reserve_balance=None):
+        self.userid = userid
         self.password = password
+        if account_balance is None:
+            account_balance = Money(0, 0)
+        if reserve_balance is None:
+            reserve_balance = Money(0, 0)
         self._account_balance_dollars = account_balance.dollars
         self._account_balance_cents = account_balance.cents
         self._reserve_balance_dollars = reserve_balance.dollars
         self._reserve_balance_cents = reserve_balance.cents
 
     def __repr__(self):
-       return "<Client('%s', balance=%s)>" % (self.username, self.account_balance)
+        return "<User('%s', balance=%s)>" % (self.userid, self.account_balance)
 
 
 class Query(Base):
     __tablename__ = 'queries'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id  = Column(Integer)
+    user_id = Column(Integer)
     stock_symbol = Column(String(7))
-    _stock_value_dollars  = Column(Integer)
-    _stock_value_cents  = Column(Integer)
+    _stock_value_dollars = Column(Integer)
+    _stock_value_cents = Column(Integer)
     stock_value = composite(Money, _stock_value_dollars, _stock_value_cents)
-    crypto_key   = Column(String(50))
-    _query_fee_dollars    = Column(Integer)
-    _query_fee_cents    = Column(Integer)
+    crypto_key = Column(String(50))
+    _query_fee_dollars = Column(Integer)
+    _query_fee_cents = Column(Integer)
     query_fee = composite(Money, _query_fee_dollars, _query_fee_cents)
-    timestamp    = Column(String(50))
+    timestamp = Column(String(50))
 
     def __init__(self, user_id, stock_symbol, stock_value, crypto_key, query_fee, timestamp):
         self.user_id = user_id
@@ -79,17 +126,17 @@ class Transaction(Base):
     __tablename__ = 'transactions'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id  = Column(Integer)
+    user_id = Column(Integer)
     stock_symbol = Column(String(7))
-    _stock_value_dollars  = Column(Integer)
-    _stock_value_cents  = Column(Integer)
-    operation    = Column(String(3))
-    quantity     = Column(Integer)
-    _broker_fee_dollars   = Column(Integer)
-    _broker_fee_cents   = Column(Integer)
-    broker_fee   = composite(Money, _broker_fee_dollars, _broker_fee_cents)
+    _stock_value_dollars = Column(Integer)
+    _stock_value_cents = Column(Integer)
+    operation = Column(String(3))
+    quantity = Column(Integer)
+    _broker_fee_dollars = Column(Integer)
+    _broker_fee_cents = Column(Integer)
+    broker_fee = composite(Money, _broker_fee_dollars, _broker_fee_cents)
     processing_time = Column(String(50))
-    timestamp    = Column(String(50))
+    timestamp = Column(String(50))
 
     def __init__(self):
         pass
@@ -100,19 +147,19 @@ class SetTransaction(Base):
     __tablename__ = 'set_transactions'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id  = Column(Integer)
+    user_id = Column(Integer)
     stock_symbol = Column(String(7))
-    _stock_value_dollars  = Column(Integer)
-    _stock_value_cents  = Column(Integer)
-    stock_value  = composite(Money, _stock_value_dollars, _stock_value_cents)
-    operation    = Column(String(3))
-    quantity     = Column(Integer)
-    _broker_fee_dollars   = Column(Integer)
-    _broker_fee_cents   = Column(Integer)
-    broker_fee   = composite(Money, _broker_fee_dollars, _broker_fee_cents)
+    _stock_value_dollars = Column(Integer)
+    _stock_value_cents = Column(Integer)
+    stock_value = composite(Money, _stock_value_dollars, _stock_value_cents)
+    operation = Column(String(3))
+    quantity = Column(Integer)
+    _broker_fee_dollars = Column(Integer)
+    _broker_fee_cents = Column(Integer)
+    broker_fee = composite(Money, _broker_fee_dollars, _broker_fee_cents)
     processing_time = Column(String(50))
-    query_times  = Column(Integer)
-    timestamp    = Column(String(50))
+    query_times = Column(Integer)
+    timestamp = Column(String(50))
 
     def __init__(self):
         pass
---
+++ tests/test_transactions_methods.py
@@ -0,0 +1,14 @@
+import unittest
+from sps.transactions import server
+
+class ServerTestCase(unittest.TestCase):
+    def setUp(self):
+        # Create a TransactionServer
+        pass
+
+
+
+class TestCommandNames(ServerTestCase):
+    def runTest(self):
+        self.assertEqual(True, True, 'True is False')
+
