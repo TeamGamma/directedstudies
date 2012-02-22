c22a7186069e5f3d7769e3dc38d7b8f9c33f0c1a
robbles <odwyerrob@gmail.com>
2012-02-21 19:51:26 -0800

    test and basic impl for QUOTe

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,7 +1,8 @@
 import unittest
 from tests.utils import DatabaseTest
 from sps.transactions.commands import ADDCommand, QUOTECommand
-from sps.database.models import User
+from sps.database.models import User, Money
+from sps.quotes.client import QuoteClient, DummyQuoteClient
 
 class TestADDCommand(DatabaseTest):
     def setUp(self):
@@ -34,20 +35,32 @@ class TestADDCommand(DatabaseTest):
         self.assertEqual(user.account_balance.cents, 92)
 
 
-@unittest.skip("QUOTE command not implemented")
 class TestQUOTECommand(DatabaseTest):
     def setUp(self):
         DatabaseTest.setUp(self)
         self._user_fixture()
         self.command = QUOTECommand()
 
+        # Set the quote client to a dummy that returns predictable results
+        QuoteClient.set_quote_client(DummyQuoteClient({
+            'AAAA': Money(23, 45),
+            'BBBB': Money(85, 39),
+        }))
+
     def test_return_value(self):
         """ Should return a decimal value for the stock price """
-        retval = self.command.run(userid='user', stock_symbol='FOO')
+        retval = self.command.run(userid='user', stock_symbol='AAAA')
         self.assertRegexpMatches(retval, '[0-9]+\.[0-9][0-9]')
+        self.assertEqual(retval, '23.45')
 
     def test_nonexistent_user(self):
         """ Should return an error message if the user does not exist """
         retval = self.command.run(userid='unicorn', stock_symbol='FOO')
         self.assertEqual(retval, 'error: user does not exist\n')
+
+    def test_validates_stock_symbol_len(self):
+        """ Should return an error if the stock symbol is too long """
+        retval = self.command.run(userid='user', stock_symbol='A' * 5)
+        self.assertEqual(retval, 'error: invalid input\n')
+
 
