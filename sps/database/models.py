131da72d46204b2d00071785099e8e8c3ff81eea
robbles <odwyerrob@gmail.com>
2012-02-23 15:27:43 -0800

    changes and cleanup of models, using mixins

--- sps/database/models.py
+++ sps/database/models.py
@@ -1,14 +1,16 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import func, Column, Integer, String, DateTime
 from sqlalchemy.orm import composite
 from collections import namedtuple
+from sps.database.utils import InitMixin, ReprMixin
 
-Base = declarative_base()
+# Maximum length of stock symbol string
+STOCK_SYMBOL_LENGTH = 8
 
 class Money(namedtuple('Money', 'dollars cents')):
     """ 
     Used to represent money in the system without floating point errors.
-    Note: Money objects are immutable 
+    Note: Money objects are immutable.
     (you can't assign dollars and cents directly)
 
     >>> m1 = Money(dollars=10, cents=20)
@@ -49,7 +51,7 @@ class Money(namedtuple('Money', 'dollars cents')):
         return Money(self.dollars - money.dollars, self.cents - money.cents)
 
     def __str__(self):
-        """ 
+        """
         Converts a Money object into a string representation.
         """
         return "%d.%02d" % (self.dollars, self.cents)
@@ -66,101 +68,105 @@ class Money(namedtuple('Money', 'dollars cents')):
         return Money(dollars, cents)
 
 
+# Base class for all SQLAlchemy models
+Base = declarative_base()
 
-class User(Base):
-    __tablename__ = 'user'
+
+class User(InitMixin, ReprMixin, Base):
+    """
+    A user of the system, with a primary and reserve account.
+    """
+    __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     userid = Column(String(50))
     password = Column(String(50))
-    _account_balance_dollars = Column(Integer)
-    _account_balance_cents = Column(Integer)
-    _reserve_balance_dollars = Column(Integer)
-    _reserve_balance_cents = Column(Integer)
+    _account_balance_dollars = Column(Integer, default=0)
+    _account_balance_cents = Column(Integer, default=0)
+    _reserve_balance_dollars = Column(Integer, default=0)
+    _reserve_balance_cents = Column(Integer, default=0)
 
     # Automatically convert between Money and dollars/cents columns
-    account_balance = composite(Money, _account_balance_dollars, _account_balance_cents)
-    reserve_balance = composite(Money, _reserve_balance_dollars, _reserve_balance_cents)
-
-    def __init__(self, userid, password, account_balance=None, reserve_balance=None):
-        self.userid = userid
-        self.password = password
-        if account_balance is None:
-            account_balance = Money(0, 0)
-        if reserve_balance is None:
-            reserve_balance = Money(0, 0)
-        self._account_balance_dollars = account_balance.dollars
-        self._account_balance_cents = account_balance.cents
-        self._reserve_balance_dollars = reserve_balance.dollars
-        self._reserve_balance_cents = reserve_balance.cents
-
-    def __repr__(self):
-        return "<User('%s', balance=%s)>" % (self.userid, self.account_balance)
+    account_balance = composite(Money,
+            _account_balance_dollars, _account_balance_cents)
+    reserve_balance = composite(Money,
+            _reserve_balance_dollars, _reserve_balance_cents)
 
 
-class Query(Base):
+
+class Query(InitMixin, ReprMixin, Base):
+    """
+    The result of a query for a stock symbol at a given time.
+    """
     __tablename__ = 'queries'
 
+    _stock_value_dollars = Column(Integer, default=0)
+    _stock_value_cents = Column(Integer, default=0)
+    _query_fee_dollars = Column(Integer, default=0)
+    _query_fee_cents = Column(Integer, default=0)
+
     id = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(Integer)
-    stock_symbol = Column(String(7))
-    _stock_value_dollars = Column(Integer)
-    _stock_value_cents = Column(Integer)
+    stock_symbol = Column(String(STOCK_SYMBOL_LENGTH))
     stock_value = composite(Money, _stock_value_dollars, _stock_value_cents)
     crypto_key = Column(String(50))
-    _query_fee_dollars = Column(Integer)
-    _query_fee_cents = Column(Integer)
     query_fee = composite(Money, _query_fee_dollars, _query_fee_cents)
-    timestamp = Column(String(50))
 
-    def __init__(self, user_id, stock_symbol, stock_value, crypto_key, query_fee, timestamp):
-        self.user_id = user_id
-        self.stock_symbol = stock_symbol
-        self.stock_value = stock_value
-        self.crypto_key = crypto_key
-        self.query_fee = query_fee
-        self.timestamp = timestamp
+    # Auto-set timestamp when created
+    query_time = Column(DateTime, default=func.now())
 
 
-class Transaction(Base):
+
+class Transaction(InitMixin, ReprMixin, Base):
+    """
+    A transaction record created by the BUY or SELL commands.
+    """
     __tablename__ = 'transactions'
 
+    _stock_value_dollars = Column(Integer, default=0)
+    _stock_value_cents = Column(Integer, default=0)
+    _broker_fee_dollars = Column(Integer, default=0)
+    _broker_fee_cents = Column(Integer, default=0)
+
     id = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(Integer)
-    stock_symbol = Column(String(7))
-    _stock_value_dollars = Column(Integer)
-    _stock_value_cents = Column(Integer)
+    stock_symbol = Column(String(STOCK_SYMBOL_LENGTH))
     operation = Column(String(3))
     quantity = Column(Integer)
-    _broker_fee_dollars = Column(Integer)
-    _broker_fee_cents = Column(Integer)
     broker_fee = composite(Money, _broker_fee_dollars, _broker_fee_cents)
+    stock_value = composite(Money, _stock_value_dollars, _stock_value_cents)
     processing_time = Column(String(50))
-    timestamp = Column(String(50))
 
-    def __init__(self):
-        pass
-
+    # Auto-set timestamp when created
+    creation_time = Column(DateTime, default=func.now())
 
 
-class SetTransaction(Base):
+
+class SetTransaction(InitMixin, ReprMixin, Base):
+    """
+    A transaction record for a trigger created by the SET_BUY_TRIGGER or
+    SET_SELL_TRIGGER commands.
+    """
     __tablename__ = 'set_transactions'
 
+    BUY = 0
+    SELL = 1
+
+    _stock_value_dollars = Column(Integer, default=0)
+    _stock_value_cents = Column(Integer, default=0)
+    _broker_fee_dollars = Column(Integer, default=0)
+    _broker_fee_cents = Column(Integer, default=0)
+
     id = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(Integer)
-    stock_symbol = Column(String(7))
-    _stock_value_dollars = Column(Integer)
-    _stock_value_cents = Column(Integer)
+    stock_symbol = Column(String(STOCK_SYMBOL_LENGTH))
     stock_value = composite(Money, _stock_value_dollars, _stock_value_cents)
-    operation = Column(String(3))
+    operation = Column(Integer)
     quantity = Column(Integer)
-    _broker_fee_dollars = Column(Integer)
-    _broker_fee_cents = Column(Integer)
     broker_fee = composite(Money, _broker_fee_dollars, _broker_fee_cents)
     processing_time = Column(String(50))
     query_times = Column(Integer)
-    timestamp = Column(String(50))
 
-    def __init__(self):
-        pass
+    # Auto-set timestamp when created
+    creation_time = Column(DateTime, default=func.now())
 
