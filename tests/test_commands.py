678ce7e61c9403cd0330dc9fa97de45b2c697400
robbles <odwyerrob@gmail.com>
2012-02-24 17:39:01 -0800

    added testing for SELL command

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,6 +1,6 @@
 import unittest
 from tests.utils import DatabaseTest
-from sps.transactions.commands import ADDCommand, QUOTECommand
+from sps.transactions.commands import ADDCommand, QUOTECommand, SELLCommand
 from sps.database.models import User, Money
 from sps.quotes.client import QuoteClient, DummyQuoteClient
 
@@ -58,9 +58,39 @@ class TestQUOTECommand(DatabaseTest):
         retval = self.command.run(userid='unicorn', stock_symbol='FOO')
         self.assertEqual(retval, 'error: user does not exist\n')
 
-    def test_validates_stock_symbol_len(self):
+    def test_validates_stock_symb)l_len(self):
         """ Should return an error if the stock symbol is too long """
         retval = self.command.run(userid='user', stock_symbol='A' * 5)
         self.assertEqual(retval, 'error: invalid input\n')
+
+class TestSELLCommand(DatabaseTest)       
+    def setUp(self):
+        # set up the database as inherited from DatabaseTest
+        DatabaseTest.setUp(self)
+        # call user fixture to populate the database (from DatabaseTest)
+        self._user_fixture()
+        #associate the sell command
+        self.command = SELLCommand()
+
+        ######################
+        #put something here to give 'user' 10 units of 'AAAA' stock
+        ##################
+
+    def test_successful_return_value(self):
+        retval = self.command.run(userid='user', stock_symbol='AAAA', \
+                amount='5')
+        self.assertEqual(retval,'success\n')
+
+    def test_too_little_stock_to_sell(self):
+        retval = self.command.run(userid='user', stock_symbol='AAAA', \
+                amount='100000')
+        self.assertEqual(retval, 'error: too little stock to sell\n') 
+
+    def test_wrong_user_id(self):
+        retval = self.command.run(userid='garbage', stock_symbol='AAAA',\
+                amount='5')
+        self.assertEqual(retval, 'error: invalid user id')
+
+
 
 
